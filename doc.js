d6f2dc7a42e7c5847fc9f33a0cc9eb8f2b0944ce
Saksham Garg <77485481+Saksham-garg@users.noreply.github.com>
2023-06-09 11:05:47 +0530

    added feature to delete and add list to items

--- doc.js
+++ doc.js
@@ -41,47 +41,127 @@
 //     even[i].style.backgroundColor = 'green';
 // }
 
+
 // TRAVERSE THE DOM
 
-var listItems = document.querySelector('.list-items');
-console.log(listItems.parentElement)
-console.log(listItems.parentNode)
+// var listItems = document.querySelector('.list-items');
+// console.log(listItems.parentElement)
+// console.log(listItems.parentNode)
+
 
 // ChildNodes
 
-console.log(listItems.childNodes)
-console.log(listItems.children)
+// console.log(listItems.childNodes)
+// console.log(listItems.children)
+
 
 // FirstChild
-console.log(listItems.firstChild)
-console.log(listItems.firstElementChild)
+// console.log(listItems.firstChild)
+// console.log(listItems.firstElementChild)
+
 
 //lastChild
-console.log(listItems.lastChild)
-console.log(listItems.lastElementChild)
+// console.log(listItems.lastChild)
+// console.log(listItems.lastElementChild)
+
 
 //nextSibling
-console.log(listItems.nextSibling)
-console.log(listItems.nextElementSibling)
+// console.log(listItems.nextSibling)
+// console.log(listItems.nextElementSibling)
+
 
 //previousSibling
-console.log(listItems.previousSibling)
-console.log(listItems.previousElementSibling)
+// console.log(listItems.previousSibling)
+// console.log(listItems.previousElementSibling)
+
 
 // Create Element
-var newDiv = document.createElement('div');
-newDiv.className = 'hello';
-newDiv.id = 'hello1';   
-newDiv.setAttribute('title','Hello Div');
-console.log(newDiv);
+// var newDiv = document.createElement('div');
+// newDiv.className = 'hello';
+// newDiv.id = 'hello1';   
+// newDiv.setAttribute('title','Hello Div');
+// console.log(newDiv);
+
 
 // Create Text Node
-var newDivText = document.createTextNode('Hello World');
-newDiv.appendChild(newDivText);
-console.log(newDiv);
 
-var container = document.querySelector('header .main-title');
-var h1 = document.querySelector('header h1');
+// var newDivText = document.createTextNode('Hello World');
+// newDiv.appendChild(newDivText);
+// console.log(newDiv);
+
+// var container = document.querySelector('header .main-title');
+// var h1 = document.querySelector('header h1');
+
 
 // Inserting into DOM
-container.insertBefore(newDivText,h1);
+// container.insertBefore(newDivText,h1);
+
+//EVENTS
+
+// document.getElementById('btn-id').addEventListener('click',buttonclick)
+
+// function buttonclick(e){
+//     console.log("Hello");
+//     console.log(e.target);
+//     console.log(e.clientX);
+//     console.log(e.clientY);
+//     console.log(e.offsetX);
+//     console.log(e.offsetY);
+//    console.log(e.altKey)
+//    console.log(e.ctrlKey)
+//    console.log(e.shiftKey)
+// }
+
+const inputField = document.querySelector("#add-list-input")
+console.log(inputField.value)
+
+const submitItem = document.querySelector("input[type='submit']")
+submitItem.addEventListener('click',addItem)
+
+const selectImage = document.querySelectorAll('.list-items-li button')
+
+const searchInput = document.getElementById('search-input')
+searchInput.addEventListener('keyup',searchItems)
+
+for(let i=0;i<selectImage.length;i++){
+    selectImage[i].addEventListener('click',deleteItem)
+}
+
+function addItem(e){
+    e.preventDefault();
+    if(inputField.value != ''){
+        const newListItem = document.createElement('li')
+        newListItem.className = 'list-items-li'
+        const newListItemText = document.createTextNode(inputField.value)
+        newListItem.appendChild(newListItemText);
+        const createButton = document.createElement('button')
+        const imageTag = document.createElement('img')
+        imageTag.setAttribute('src','./close.png')
+        createButton.appendChild(imageTag)
+        const listItems = document.getElementById('list')
+        listItems.appendChild(newListItem)
+        newListItem.appendChild(createButton)
+        inputField.value = ''
+        }
+    else{
+            alert("Please enter some text to add to  list");
+        }
+}
+
+function deleteItem(e){
+    e.target.parentNode.parentNode.remove()
+}
+
+function searchItems(e){
+    const listItem = document.getElementsByClassName('list-items-li')
+    console.log(listItem)
+    const text = e.target.value.toLowerCase();
+
+    Array.from(listItem).forEach(function(item){
+        if(item.textContent.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block'
+        }else{
+            item.style.display = 'none';
+        }
+    })
+}
